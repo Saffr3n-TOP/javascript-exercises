85fff11597da6be6a263a9998589a8c882a0dedf
Saffr3n-TOP <saffr3n.reserve@gmail.com>
2023-10-14 16:57:09 +0300

    Complete fifth exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function(x, y) {
+  if (typeof x !== 'number' || typeof y !== 'number' || x < 0 || y < 0) {
+    return 'ERROR';
+  }
 
+  const min = x < y ? x : y;
+  const max = x > y ? x : y;
+
+  let res = 0;
+  for (let i = min; i <= max; i++) {
+    res += i;
+  }
+
+  return res;
 };
 
 // Do not edit below this line
