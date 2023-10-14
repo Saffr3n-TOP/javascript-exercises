b3d949e796c4b7089849f79c5829a6d3ad833859
Saffr3n-TOP <saffr3n.reserve@gmail.com>
2023-10-14 16:33:26 +0300

    Complete third exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+  const arr = str.split('');
+  arr.sort(() => -1);
+  return arr.join('');
 };
 
 // Do not edit below this line
