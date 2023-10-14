16364114aec79024192294fa2218b2f89ba5956c
Saffr3n-TOP <saffr3n.reserve@gmail.com>
2023-10-14 16:46:58 +0300

    Complete fourth exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...rest) {
+  const res = [];
 
+  for (let i = 0; i < arr.length; i++) {
+    if (rest.includes(arr[i])) continue;
+    res.push(arr[i]);
+  }
+
+  return res;
 };
 
 // Do not edit below this line
