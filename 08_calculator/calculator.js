1a35fe472a07eb78808b617cc1a96ac0b78ffe48
Saffr3n-TOP <saffr3n.reserve@gmail.com>
2023-10-15 21:24:05 +0300

    Complete eighth exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function(x, y) {
+	return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+	return x - y;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	let res = 0;
+  arr.forEach(num => (res += num));
+  return res;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  if (!arr.length) return 0;
+  let res = arr[0];
+  for (let i = 1; i < arr.length; i++) {
+    res *= arr[i]
+  }
+  return res;
 };
 
-const power = function() {
-	
+const power = function(num, pow) {
+	let res = num;
+  for (let i = 1; i < pow; i++) {
+    res *= num;
+  }
+  return res;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let res = 1;
+  if (num === 0) return res;
+  for (let i = 1; i < num; i++) {
+    res *= i + 1;
+  }
+  return res;
 };
 
 // Do not edit below this line
