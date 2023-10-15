aeaa7753844da2c8f5976b2f8f7d13515835b2da
Saffr3n-TOP <saffr3n.reserve@gmail.com>
2023-10-15 21:52:43 +0300

    Complete tenth exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+  if (+num <= 0) return 'OOPS';
 
+  let memo;
+  let curr = 1;
+  let next = 1;
+
+  for (let i = 1; i < num; i++) {
+    memo = next;
+    next += curr;
+    curr = memo;
+  }
+
+  return curr;
 };
 
 // Do not edit below this line
