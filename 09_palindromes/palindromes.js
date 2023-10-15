a4fce3d499c7f4630f251349d7a039aac41e2357
Saffr3n-TOP <saffr3n.reserve@gmail.com>
2023-10-15 21:40:50 +0300

    Complete nineth exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  let arr = str.toUpperCase().split('');
+  arr = arr.filter(symbol => /[A-Za-z0-9]/.test(symbol));
+  return arr.join('') === arr.reverse().join('');
 };
 
 // Do not edit below this line
