ed87914163389b98e7e268d5e8fdda9cdd77eb6c
Saffr3n-TOP <saffr3n.reserve@gmail.com>
2023-10-15 22:48:53 +0300

    Complete twelweth exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
+const findTheOldest = function (arr) {
+  return arr.reduce((prevPerson, currPerson) => {
+    const prevPersonAge =
+      (prevPerson.yearOfDeath || new Date().getFullYear()) -
+      prevPerson.yearOfBirth;
 
+    const currPersonAge =
+      (currPerson.yearOfDeath || new Date().getFullYear()) -
+      currPerson.yearOfBirth;
+
+    return prevPersonAge > currPersonAge ? prevPerson : currPerson;
+  });
 };
 
 // Do not edit below this line
